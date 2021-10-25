f06119a85d47d2e5fe54f20246ec043bff40840b
Fayas Noushad <TheFayasNoushad@gmail.com>
2021-10-25 15:27:03 +0530

    Added domains extract from a list

--- domain_extract.py
+++ domain_extract.py
@@ -1,3 +1,10 @@
 def domain(url):
     domain = url.split("://", 1)[1].split("/", 1)[0]
     return domain
+
+
+def domains(urls):
+    domains = []
+    for url in urls:
+        domains.append(domain(url))
+    return domains
