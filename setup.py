a7e72fe20a8d706e1fd3469bcf931279745bd15a
Fayas Noushad <TheFayasNoushad@gmail.com>
2021-11-20 17:24:02 +0530

    Update setup (#8)

--- setup.py
+++ setup.py
@@ -1,9 +1,8 @@
 import os
-import pathlib
-from setuptools import setup, find_packages
+import setuptools
 
 
-def requirements(file="requirements.txt") -> list:
+def requirements(file="requirements.txt"):
     if os.path.isfile(file):
         with open(file, encoding="utf-8") as r:
             return [i.strip() for i in r]
@@ -19,21 +18,31 @@ def readme(file="README.md"):
         return ""
 
 
-setup(
+setuptools.setup(
     name="Domain-Extract",
-    version="1.0.4",
-    author="FayasNoushad",
-    long_description=README,
+    version="1.0.5",
+    description="Domain extract from string",
+    long_description=readme(),
     long_description_content_type="text/markdown",
-    description="A domain extract python module",
+    url="https://github.com/FayasNoushad/",
+    download_url="https://github.com/FayasNoushad/Domain-Extract/releases/latest",
     license="MIT",
-    url="https://github.com/FayasNoushad/Domain-Extract",
+    author="Fayas Noushad",
+    author_email="contact@fayas.me",
     classifiers=[
-        "Programming Language :: Python :: 3",
+        "Intended Audience :: Developers",
+        "Natural Language :: English",
+        "Programming Language :: Python",
         "License :: OSI Approved :: MIT License",
-        "Operating System :: OS Independent",
+        "Operating System :: OS Independent"
     ],
-    packages=find_packages(),
-    install_requires=requirements(),
-    python_requires=">=3.6"
+    project_urls={
+        "Tracker": "https://github.com/FayasNoushad/Domain-Extract/issues",
+        "Source": "https://github.com/FayasNoushad/Domain-Extract",
+        "Documentation": "https://domain-extract.projects.fayas.me",
+    },
+    python_requires=">=3.6",
+    packages=setuptools.find_packages(),
+    zip_safe=False,
+    install_requires=requirements()
 )
